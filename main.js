79850559da55b356fc88698ecd68efa7b70a28c9
ByLeo-native <heyleoqueonda1@gmail.com>
2023-01-24 15:12:42 -0300

    Aniadiendo slider de carrito, ademas se agrego funcion para cerrar  otros slider al intentar abrir uno

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
-}
-
 const burguerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
-burguerIcon.addEventListener("click", toggleMobileMenu);
+const slidingElements = [ desktopMenu, mobileMenu, productDetail ];
 
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive");
+menuEmail.addEventListener("click", ()=> { toggleSlider(desktopMenu);});
+burguerIcon.addEventListener("click", ()=> { toggleSlider(mobileMenu);});
+navbarShoppingCart.addEventListener("click", ()=> { toggleSlider(productDetail);} );
+
+function closeSliders (element) {
+    slidingElements.forEach( slider => {
+        if(element != slider) {
+            slider.classList.add("inactive");
+        }
+    });
+}
+
+function toggleSlider(slider) {
+    closeSliders(slider);
+    slider.classList.toggle("inactive");
 }
